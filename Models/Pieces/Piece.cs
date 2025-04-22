0e061eae451a370d7ee7b57d7750f7e9e55b84fc
nithish124 <nithishkumar@admindroid.com>
2025-04-23 00:42:55 +0530

    Feat: Add Piece abstract class to describe the chess pieces properties and behaviours.

---
+++ Models/Pieces/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Models.Pieces
+{
+    public abstract class Piece
+    {
+        public string Position { get; set; }
+        public PieceType Type { get; set; }
+        public Piece(string position, PieceType type)
+        {
+            Position = position;
+            Type = type;
+        }
+
+        public abstract List<string> GetPossibleMoves(IChessBoard board);
+    }
+}
