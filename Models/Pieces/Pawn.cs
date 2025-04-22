688c92a7eaf108b2d2071bf222cc7f65d3a7df2b
nithish124 <nithishkumar@admindroid.com>
2025-04-23 00:44:05 +0530

    Add implementation for pawn Chess Piece.

---
+++ Models/Pieces/Pawn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Models.Pieces
+{
+    internal class Pawn : Piece
+    {
+        public Pawn(string position) : base(position, PieceType.Pawn)
+        {
+        }
+
+        public override List<string> GetPossibleMoves(IChessBoard board)
+        {
+            var moves = new List<string>();
+            // As stated in the requirement, we assume pawn moves only one step forward.
+            int row = Position[1] - '1';  
+            char col = Position[0];       
+
+            if (row < board.GetRow())
+                moves.Add($"{col}{row + 2}");  
+
+            return moves;
+        }
+    }
+}
