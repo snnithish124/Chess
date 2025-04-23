3cd529c659566c9b6a271bf843fd91a64ca63e0c
nithish124 <nithishkumar@admindroid.com>
2025-04-23 12:09:56 +0530

    Feat: Add queen piece and required behaviours

---
+++ Models/Pieces/Queen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Models.Pieces
+{
+    internal class Queen : Piece
+    {
+
+        public Queen(string position) : base(position, PieceType.Queen)
+        {
+        }
+
+        public override List<string> GetPossibleMoves(IChessBoard board)
+        {
+            var moves = new List<string>();
+
+            foreach (var (rowOffset, colOffset) in board.GetAllDirections())
+            {
+                int toRow = curRow + rowOffset;
+                int toCol = curCol + colOffset;
+
+                while (board.IsInsideBoard(toRow, toCol))
+                {
+                    moves.Add(board.ToPosition(toRow, toCol));
+                    toRow += rowOffset;
+                    toCol += colOffset;
+                }
+
+            }
+            return moves;
+        }
+
+    }
+}
