012a47af1091b335b5f63ac190a7e153678bccb5
nithish124 <nithishkumar@admindroid.com>
2025-04-23 22:55:09 +0530

    Refactor: Move to different functions for better maintability

--- Program.cs
+++ Program.cs
@@ -3,48 +3,24 @@ using Chess.Models.Pieces;
 using Chess.Utils;
 
 var inputString = string.Empty;
-
+Console.WriteLine("This App will give the possible moves that a piece can take in a chess board. \nYou can give the piece and its current position to get the details.");
 do
 {
     try
     {
+        Console.WriteLine("---------------------------------------------------------------------------------");
         Console.Write("Enter Position(Ex: Pawn, H2) or Enter Exit: ");
         inputString = Console.ReadLine();
+        if (inputString == null) continue;
         if (inputString.ToLower() == "exit") break; //Checking as case insensitive format
 
         var input = inputString.Split(',');
         if (input.Length != 2)
         {
-            Console.WriteLine("Input is not in the format, Give as comma separated");
+            Console.WriteLine("Input is not in the format, Give as comma separated.\n");
             continue;
         }
-
-        IChessBoard board = new BasicChessBoard();
-        var ChessPiece = input[0].Trim().ToLower();
-
-        Piece piece = null;
-        if (ChessPiece == "king")
-        {
-            piece = new King(input[1].Trim().ToUpper());
-        }
-        else if (ChessPiece == "pawn")
-        {
-            piece = new Pawn(input[1].Trim().ToUpper());
-        }
-        else if (ChessPiece == "queen")
-        {
-            piece = new Queen(input[1].Trim().ToUpper());
-        }
-        else
-        {
-            Console.WriteLine("Given Piece Not Found");
-            continue;
-        }
-
-        if (board.IsInsideBoard(piece.curRow, piece.curCol))
-            piece.GetPossibleMoves(board).print();
-        else
-            Console.WriteLine("Piece Is Placed Outside The Board");
+        GetMoves(input);
     }
     catch(Exception e)
     {
@@ -54,3 +30,46 @@ do
 }
 while (true);
 
+void GetMoves(string[] input)
+{
+
+    IChessBoard board = new BasicChessBoard();
+    try
+    {
+        Piece piece = GetPiece(input);
+        if (board.IsInsideBoard(piece.curRow, piece.curCol))
+            piece.GetPossibleMoves(board).print();
+        else
+            Console.WriteLine("Piece Is Placed Outside The Board.\n");
+    }
+    catch(KeyNotFoundException e )
+    {
+        throw e;
+    }
+}
+/// <summary>
+/// Performs operation that might fail.
+/// </summary>
+/// <exception cref="KeyNotFoundException">Thrown when the piece not found.</exception>
+Piece GetPiece(string[] input)
+{
+
+    var ChessPiece = input[0].Trim().ToLower();
+
+    if (ChessPiece == "king")
+    {
+       return new King(input[1].Trim().ToUpper());
+    }
+    else if (ChessPiece == "pawn")
+    {
+        return new Pawn(input[1].Trim().ToUpper());
+    }
+    else if (ChessPiece == "queen")
+    {
+        return new Queen(input[1].Trim().ToUpper());
+    }
+    else
+    {
+        throw new KeyNotFoundException("Piece Not Found. Only King, Pawn and Queen are available.\n");
+    }
+}
