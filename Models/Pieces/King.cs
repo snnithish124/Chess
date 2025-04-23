5c58efcce7981d5a053d93186ff4834c2a5632bf
nithish124 <nithishkumar@admindroid.com>
2025-04-23 12:08:26 +0530

    Feat: Add king piece and necessary methods

---
+++ Models/Pieces/King.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Models.Pieces
+{
+    internal class King : Piece
+    {
+        public King(string position) : base(position, PieceType.King)
+        {
+
+        }
+
+        public override List<string> GetPossibleMoves(IChessBoard board)
+        {
+            var moves = new List<string>();
+            
+
+            if (board.IsInsideBoard(curCol, curRow))
+            {
+                var directions = board.GetAllDirections();
+                for (int i = 0; i < directions.Length; i++)
+                {
+                    int toRow = curRow + directions[i].row;
+                    int toCol = curCol + directions[i].col;
+                    if (board.IsInsideBoard(toRow, toCol))
+                        moves.Add(board.ToPosition(toRow, toCol));
+                }
+            }
+            return moves;
+        }
+    }
+}
